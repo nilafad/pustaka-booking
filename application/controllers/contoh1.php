7131711842851754bb2fb3c6b3a866202af1671b
nilafad <nilafadhila22@gmail.com>
2021-09-26 15:55:23 +0700

    pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenakan</h1>";
+        echo "Nama saya Nila Fadhila <br>
+        Saya tinggal di daerah Condet <br>
+        Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
---
+++ application/controllers/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming";
+    }
+
+    public function penjumlahan($nil1, $nil2)
+    {
+        $this->load->model('Model_latihan1');
+        
+        $data['nilai1'] = $nil1;
+        $data['nilai2'] = $nil2;
+        $data['hasil'] = $this->Model_latihan1->jumlah($nil1, $nil2);
+
+        $this->load->view('view-latihan1', $data);
+    }
+}
